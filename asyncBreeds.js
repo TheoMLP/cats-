4dbf6f73a519c9ae911243730b874be8f6189292
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-27 22:42:41 +0000

    finished unit testing for synchronous and asynchronous code

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,13 +5,15 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // ISSUE: Returning from inner callback function, not our main function.
     console.log('Callback: I have the data!')
-    if (!error) callback(data); 
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    };
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so this function returns undefined.
 }
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', bombay => {
-  console.log("Return Value: ", bombay)
-}); 
+
+module.exports = breedDetailsFromFile
